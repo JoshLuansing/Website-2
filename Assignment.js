b854a6311e63c17de6bc648d8a73808623f9811d
JoshLuansing <130131383+JoshLuansing@users.noreply.github.com>
2023-04-16 23:43:21 +0930

    Add files via upload

---
+++ Assignment.js
@@ -0,0 +1,12 @@
+﻿const greetingElem = document.getElementById("greeting");
+const greetings = ["안녕하세요","Hi", "Hola", "Bonjour", "Zdravstvuyte", "Guten Tag", "こんにちは", "你好", "Olá",];
+let index = 0;
+
+setInterval(() => {
+    greetingElem.style.opacity = 0; // set opacity to 0
+    setTimeout(() => {
+        greetingElem.textContent = greetings[index];
+        greetingElem.style.opacity = 1; // set opacity to 1 after 0.5s delay
+    }, 500);
+    index = (index + 1) % greetings.length;
+}, 2000);
